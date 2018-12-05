6bf958db4793bafa17e6ce66e7fe372dbdafbb9d
Jessica Anderson <jessie.leigh30@gmail.com>
2018-12-05 15:56:17 -0700

    cards show

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cards from "./Cards";
+import { Container, Header, Card, Button } from "semantic-ui-react";
+
+
 
 class App extends Component {
+  state = {
+    cards: [
+      {id: 1, frontQuestion: "What is Jessica's dog's name?", backAnswer: "Baymax",},
+      {id: 1, frontQuestion: "What is Jessica's cat's name?", backAnswer: "Cosmo",},
+      {id: 1, frontQuestion: "How old is Baymax", backAnswer: "3",},
+    ],
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Header as= "h1"> React Flash Cards</Header>
+        <Card.Group> 
+          <Cards varCards={this.state.cards}>
+          </Cards>
+        </Card.Group>
+      </Container>
+
     );
   }
 }
---
+++ src/Cards.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, Button } from 'semantic-ui-react';
+import flashCard from "./flashCard";
+
+const Cards = ({varCards,}) => (
+   //render data here //semantic card area
+   
+     
+
+ 
+)
+
+export default Cards;
+
+
+
+
+
+
+
+
+
+
+
+
